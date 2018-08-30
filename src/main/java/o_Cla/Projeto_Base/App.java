891312975d67c7246a0bf5d63c07387fa12bbcf7
Stk <igor.stk00@gmail.com>
2018-08-30 14:22:26 -0300

    Projeto Base

---
+++ src/main/java/o_Cla/Projeto_Base/App.java
@@ -0,0 +1,13 @@
+package o_Cla.Projeto_Base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/o_Cla/Projeto_Base/TesteParametrizado.java
@@ -0,0 +1,14 @@
+package o_Cla.Projeto_Base;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TesteParametrizado {
+
+	@Test
+	void test() {
+		assertEquals(2, 1+1);
+	}
+
+}
